e9fa88cc960226fa610d39665717a03bee1f6330
reinout91 <reinout91@gmail.com>
2020-09-15 00:03:27 +0200

    Update and rename testBlah.cpp to testSum.cpp

--- {{cookiecutter.repo_name}}/test/testBlah.cpp
+++
@@ -1,5 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(testBlah, OutputTest) {
-    EXPECT_EQ(1,1);
-}
---
+++ {{cookiecutter.repo_name}}/test/testSum.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(testSum, OutputTest) {
+    EXPECT_EQ(4, sum(1,3));
+}
