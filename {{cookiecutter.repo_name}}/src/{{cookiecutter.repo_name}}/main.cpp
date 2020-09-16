dd7b41285126d51bef2c7d489da22ce1a2c35bf6
reinout91 <reinout91@gmail.com>
2020-09-16 22:40:19 +0200

    Update main.cpp

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/main.cpp
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <{{cookiecutter.repo_name}}_utils.h>
 #include <fmt/core.h> //packagemanager downloaded library.
-using namespace std;
+namespace std{
 
 int main()
 {
@@ -9,3 +9,4 @@ int main()
   std::cout << message <<std::endl;
 return 0;
 }
+};
