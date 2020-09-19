19311c08937dd1e1cab11dfc27217d9ecbed01ac
reinout91 <reinout91@gmail.com>
2020-09-19 19:45:29 +0200

    Update main.cpp

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/main.cpp
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
-#include <{{cookiecutter.repo_name}}_utils.h>
+#include <matrix_example_utils.h>
 #include <fmt/core.h> //packagemanager downloaded library.
+#include <armadillo> //system wide installed package.
+
 using namespace std;
+using namespace arma;
 
 int main()
 {
   auto message = fmt::format("The answer is {}", sum(1,2));
   std::cout << message <<std::endl;
+  mat v = randu<mat>(3,3);
+  mat b = randu<mat>(3,3);
+  std::cout << v*b << std::endl;
 return EXIT_SUCCESS;
 }
 
