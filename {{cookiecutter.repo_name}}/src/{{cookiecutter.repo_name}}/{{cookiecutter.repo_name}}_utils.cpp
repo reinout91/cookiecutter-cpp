2c39c9dce19c00b8dfa37fcafddedfa949ab1e6b
reinout91 <reinout91@gmail.com>
2020-09-23 19:19:50 +0200

    Update {{cookiecutter.repo_name}}_utils.cpp

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}_utils.cpp
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}_utils.cpp
@@ -1,4 +1,4 @@
-#include "./{{cookiecutter.repo_name}}_utils.h"
+#include "./{{cookiecutter.repo_name}}Utils.h"
 int sum(int a, int b)
 {
   std::cout << "input a: " << a << std::endl << "input b: " << b << std::endl;
