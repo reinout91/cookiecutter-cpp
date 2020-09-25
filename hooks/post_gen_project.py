63b45606ee91d84376776058fb0c97787b06e534
reinout91 <reinout91@gmail.com>
2020-09-25 17:02:45 +0200

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     subprocess.call(('git', 'checkout', '-b', branch))
     subprocess.call(('git', 'remote', 'add', remote, url))
     if os.name == 'nt':
-        shutil.move('pre-commit', '.git\\hooks\\')
+        shutil.move('pre-commit', '.git\\hooks')
     else:
         subprocess.call(('mv', 'pre-commit', '.git/hooks/'))
         
