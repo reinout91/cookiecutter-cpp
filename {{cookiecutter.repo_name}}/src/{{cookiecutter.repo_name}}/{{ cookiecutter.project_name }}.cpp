e1b32f287c66fe419f705a71f3db15f99b893716
reinout91 <reinout91@gmail.com>
2020-10-15 15:21:40 +0200

    Update {{ cookiecutter.project_name }}.cpp

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/{{ cookiecutter.project_name }}.cpp
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.repo_name}}/{{ cookiecutter.project_name }}.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <{{ cookiecutter.project_name }}Utils.h>
 #include <fmt/core.h> //packagemanager downloaded library.
-#define ARMA_DONT_USE_WRAPPER
-#define ARMA_USE_LAPACK
-#define ARMA_USE_BLAS
+//#define ARMA_DONT_USE_WRAPPER
+//#define ARMA_USE_LAPACK
+//#define ARMA_USE_BLAS
 #include <armadillo> //system wide installed package.
 
 using namespace std;
